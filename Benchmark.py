c9e91924c20931324bc21010ae9278cdc7bcc0c6
lean-he <leanhemmers@gmail.com>
2023-12-23 15:02:20 +0100

    Update Benchmark.py to support command line arguments

--- Benchmark.py
+++ Benchmark.py
@@ -1,10 +1,10 @@
+import argparse
+import ast
 import matplotlib.pyplot as plt
 import os
 import random
 import subprocess
 import time
-
-n = 10000
 
 run_files = ["Bubblesort", "Bubblesort.class", "Bubblesort.py",
              "Quicksort", "Quicksort.class", "Quicksort.py",
@@ -60,21 +60,40 @@ def make_result_plot(results):
     plt.ylabel("time (in seconds)")
     plt.title("Selectionsort")
 
-    plt.suptitle("Benchmark of sorting algorithms (" + str(n) + " elements in random order)")
+    plt.suptitle("Benchmark of sorting algorithms (" + str(args.n) + " elements in random order)")
     plt.savefig("result.png")
     plt.close()
+
+parser = argparse.ArgumentParser(
+    description="Bechmarking tool of sorting algorithms that creates presentable plots.")
+parser.add_argument("--no-compile", action="store_true",
+                    help="no compilation of the source files. For a correct functionality is a compilation on the first execution needed.")
+parser.add_argument("--no-number-gen", action="store_true",
+                    help="no generation of a new data set. The last used one will be used again. On the first execution is a generation however necessary.")
+parser.add_argument("-n", type=int, required=False, default=10000, help="N specifies the size of random elements that get sorted. The default value is 10000.")
+args = parser.parse_args()
 
 os.makedirs("build/c", exist_ok=True)
 os.makedirs("build/java", exist_ok=True)
 os.makedirs("data", exist_ok=True)
 
-for i in os.listdir("src/c") + os.listdir("src/java"):
-    compile_code(i)
+if (not args.no_compile):
+    for i in os.listdir("src/c") + os.listdir("src/java"):
+        compile_code(i)
 
-numbers = generate_data(n)
-f = open("data/numbers.txt", "w")
-f.write(str(numbers))
-f.close()
+if (not args.no_number_gen):
+    numbers = generate_data(args.n)
+    f = open("data/numbers.txt", "w")
+    f.write(str(numbers))
+    f.close()
+else:
+    f = open("data/numbers.txt", "r")
+    numbers = ast.literal_eval(f.read())
+    print(numbers)
+    print(type(numbers))
+    print(len(numbers))
+    f.close()
+    args.n = len(numbers)
 
 results = [measure_real_time(run_files[i], str(numbers)) for i in range(len(run_files))]
 
